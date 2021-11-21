5120bde13ec562fd529c6e95e865513c92d2f051
Atte-Oskari-Rasanen <70641619+Atte-Oskari-Rasanen@users.noreply.github.com>
2021-11-21 23:33:48 +0100

    Update File_organise_Segment.py
    Fixed a a few bugs and removed dead code.

--- Analysis scripts/File_organise_Segment.py
+++ Analysis scripts/File_organise_Segment.py
@@ -9,7 +9,7 @@ import shutil
 
 import numpy as np
 import matplotlib.pyplot as plt
-from smooth_tiled_divisions import *
+from smooth_tiled_divisions_edited2 import *
 
 from skimage import data
 from skimage.color import rgb2hed, hed2rgb
@@ -21,7 +21,7 @@ import os
 import ntpath
 import glob
 import tensorflow
-import tensorflow
+# import tensorflow
 import keras
 
 #Python script for renaming and reorganising the deconvolved images performed by ImageJ and then
@@ -30,9 +30,10 @@ import keras
 directory = '/home/atte/Documents/PD_images/test_decov/'
 directory = "/home/atte/Documents/PD_images/batch8_retry/"
 directory = sys.argv[1]
+print('Directory: ' + directory)
 # directory = '/home/atte/Documents/PD_images/batch8_retry/test'
 patch_size = int(sys.argv[2])
-patch_size = 736
+# patch_size = 736
 model_path = '/home/inf-54-2020/experimental_cop/scripts/Plots_Unet/Alldat_dice_ps736_bs128_ep3.h5'
 
 # segm_model = sys.argv[3]
@@ -73,7 +74,8 @@ for root, subdirectories, files in scandir.walk(directory):
     for subdir in subdirectories:
         if any(integ in subdir for integ in ints): #check that the dir name contains an int
             print('subdir: '+subdir)
-            animal_dir = main_dir + '/' + subdir
+            animal_id = 'Animal_' + subdir
+            animal_dir = main_dir + '/' + animal_id
             #under Deconvolved_im/ make a subdir for each image id
             try:
                 os.mkdir(animal_dir)
@@ -92,10 +94,7 @@ for root, subdirectories, files in scandir.walk(directory):
                 os.mkdir(hunu_ch_dir)
             except OSError:
                 print ("Creation of the directory %s failed" % hunu_ch_dir)
-            # else:
-            #     print ("Successfully created the directory %s " % hunu_ch_dir)
-            
-            # print(col1a1_ch_dir)
+
             try:
                 os.mkdir(col1a1_ch_dir)
             except OSError:
@@ -105,13 +104,7 @@ for root, subdirectories, files in scandir.walk(directory):
             
             subdir_path = root + '/' + subdir + '/'
             
-            print('root: '+root)
-            print('subdir: '+subdir)
 
-            # print('subdir files:')
-            #print(glob.glob(subdir_path))
-            # subdir_path_to_orig_ims = root + '/' + subdir 
-            # print(str(os.listdir(subdir_path)))
             
             #now you have created a directory under Deconvolved_ims with specific animal ID along with subdirs for 
             #hunu and col1a1. Now need to transfer the files from the original location to here.
@@ -133,15 +126,16 @@ for root, subdirectories, files in scandir.walk(directory):
                 id_spec = next(uniq_id) # specific id for the corresponding hunu and its col1a1 image
                 # #subdir designated the animal id, 
                 # imagename = str(subdir) + '_' +str(id_spec) +'_' + str(patch_size) + '.png'
-
+                print('subdir prior to if cond: '+ subdir)
                 # print(imagename)
                 #get the threshold
-
-                if 'col1a1' in file and not subdir in file:
+                print('im name orig: '+ imagename_orig)
+                if 'col1a1' in file and not animal_id in file: #not subdir condition to prevent stacking up the new name to several files containing the string match or processing files inside deconvolved dir
                     print('col1a1 file:' + file)
                     #give the file the specific id name
                     new_filename = str(subdir) + '_' +str(id_spec) + '_' + str(patch_size) +'_'+ imagename_orig +'.png'
                     old_file = imagepath 
+                    new_file = subdir + "/" + new_filename
                     new_file = root + '/'+subdir + "/" + new_filename
 
                     os.rename(old_file, new_file)
@@ -170,7 +164,7 @@ for root, subdirectories, files in scandir.walk(directory):
                     print('saved to ' + col1a1_ch_dir)
             
             for hunufile in os.listdir(subdir_path):
-                if 'hunu' in hunufile and not subdir in hunufile:
+                if 'hunu' in hunufile and not animal_id in hunufile:
                     imagepath=subdir_path +  hunufile
                     print('HUNU PATH: ' + imagepath)
                     print('HUNU NAME: ' + hunufile)
@@ -181,8 +175,9 @@ for root, subdirectories, files in scandir.walk(directory):
                             # print('name part: '+ name_part)
                             new_filename = col1_dic[id_part] + '_' + id_part + '_hunu.png'
                             old_file = imagepath
+                            new_file = subdir + "/" + new_filename
                             new_file = root + '/'+subdir + "/" + new_filename
-        
+
                             os.rename(old_file, new_file)
                             imagepath=new_file
         
@@ -207,42 +202,11 @@ cp_save_path = '/home/inf-54-2020/experimental_cop/scripts/transfer_learning_vgg
 model_segm = keras.models.load_model(model_path, compile=False) #need to set compile as false since we are predicting, >
 
 
-# subdirs = os.listdir(main_dir)
-# for subdir in subdirs:
-#     if not '_' in subdir:
-#         segm_dir = main_dir + '/' + subdir + '/' + segm_model +'_'+ str(patch_size) + '_Segmented'
-
-#         print('segmdir: '+ segm_dir)
-#         try:
-#             os.mkdir(segm_dir)
-#         except OSError:
-#             print ("Creation of the directory %s failed" % segm_dir)
-#         else:
-#             print ("Successfully created the directory %s " % segm_dir)
-        
-        
-# os.walk(directory)
 [x[0] for x in os.walk(directory)]
 for root, subdirectories, files in scandir.walk(main_dir):
     print(subdirectories)
     for subdir in subdirectories:
         print('subdir:' + subdir)
-
-        #first create a subdir where you save the segmented image. This dir is found inside the corresponding image to be segmented
-        # segm_dir = subdir + '_' + segm_model +'_'+ str(patch_size) + '_Segmented'
-        # segm_dir = main_dir + '/' + subdir + '_' + segm_model +'_'+ str(patch_size) + '_Segmented'
-        # segm_dir = main_dir + '/' + subdir + '/' + segm_model +'_'+ str(patch_size) + '_Segmented'
-
-        # print('segmdir: '+ segm_dir)
-        # try:
-        #     os.mkdir(segm_dir)
-        # except OSError:
-        #     print ("Creation of the directory %s failed" % segm_dir)
-        # else:
-        #     print ("Successfully created the directory %s " % segm_dir)
-        
-        # print('Number of files to process:' + file_count)
-
         subdir_path = root +'/' +subdir + '/'
         print('file:'+file)
         imagepath=root + '/'+subdir + "/" + file
@@ -259,7 +223,6 @@ for root, subdirectories, files in scandir.walk(main_dir):
         imagename = str(im_index) + '_' +str(id_hunu_col) +'_'+ subdir + '_' #+ str(patch_size)
 
         print('SUBDIR PATH FOR SEGMENTATION: ' +subdir_path)
-        #print(glob.glob(subdir_path))
         for imagefile in os.listdir(subdir_path):
             if imagefile.endswith('.png'): # or imagefile.endswith('.tif'):
                 if 'hunu' in imagefile:
@@ -268,14 +231,13 @@ for root, subdirectories, files in scandir.walk(main_dir):
         
                     imagename=ntpath.basename(imagepath)#take the name of the file from the path and save it
                     imagename_orig = imagename.split('.')[0]
-        #print(imagename)
-        #get the threshold
+
                     #apply predict_img_with_smooth_windowing function which segments the image using the trained model
                     #supplied by cutting the image into patches of the appointed size, segmenting them, then combining 
                     #the patches
                     img_segm_grids_removed = predict_img_with_smooth_windowing(img,
                         window_size=patch_size,
-                        subdivisions=2,  # Minimal amount of overlap for windowing. Must be an even number.
+                        subdivisions=4,  # Minimal amount of overlap for windowing. Must be an even number.
                         nb_classes=1,
                         pred_func=(
                             lambda img_batch_subdiv: model_segm.predict((img_batch_subdiv))
@@ -285,26 +247,7 @@ for root, subdirectories, files in scandir.walk(main_dir):
                     img_segm_grids_removed = np.squeeze(img_segm_grids_removed, axis = 2)
                     im_final = Image.fromarray((img_segm_grids_removed * 255).astype(np.uint8))
                     width, height = im_final.size
-                    # Setting the points for cropped image
-                    left = width * 0.1
-                    right = width - left
-                    top = 0
-                    bottom = height
-                    im_final = im_final.crop((left, top, right, bottom))
-                    left = 0
-                    right = width
-                    top = height * 0.1
-                    bottom = height - top
-                    im_final = im_final.crop((left, top, right, bottom))
-                    im_final 
-    
-    # top = height / 4
-    # right = 164
-    # bottom = 3 * height / 4
-    
-    # Cropped image of above dimension
-    # (It will not change original image)
-    # img_segm_cropped = img_segm.crop((left, top, right, bottom))
+  
 
                     # im_final_crop = crop_borders(im_final)
                     im_final.save(subdir_path + imagename_orig + '_Segm.png')
